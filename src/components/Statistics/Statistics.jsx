73116abc1bba2a023e7d9117df893e3da9bef20d
Yuliia Karas <iuliiakaras@gmail.com>
2023-05-28 17:10:23 +0200

    step#2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,8 +4,20 @@ import PropTypes from 'prop-types';
 
 export default class FeedbackOptions extends Component {
     constructor(props) {
-        super(props)
+        super(props);
     }
+
+    countTotalFeedback() {
+    return (
+        this.props.state.good+this.props.state.neutral+this.props.state.bad
+    )}
+    
+    countPositiveFeedbackPercentage() {
+        return (
+           Math.round(this.props.state.good/this.countTotalFeedback()*100) 
+        )
+    }
+
     render()
 
 {
@@ -14,7 +26,10 @@ export default class FeedbackOptions extends Component {
                 <h2>Statistics</h2>
                 <p>Good: { this.props.state.good}</p>
                 <p>Neutral: { this.props.state.neutral}</p>
-                <p>Bad: { this.props.state.bad}</p>
+                <p>Bad: {this.props.state.bad}</p>
+                <p>Total: {this.countTotalFeedback()}</p>
+                <p>Positive feedback:{ this.countPositiveFeedbackPercentage()}%</p>
+
                 
             </>
             
