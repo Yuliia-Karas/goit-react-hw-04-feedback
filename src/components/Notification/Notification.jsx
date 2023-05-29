b0397e975ce3e96e0a398da68ca2fc1784ca1086
Yuliia Karas <iuliiakaras@gmail.com>
2023-05-29 23:05:01 +0200

    prop#2

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,8 +1,10 @@
 import React  from 'react';
 // import css from './Notification.module.css';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 export const Notification = (props) => (
     <p>{props.message} </p>
 )
-    
+    Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
